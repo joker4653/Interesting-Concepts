b20614b960aae479546bf95ce5ba6dcdf29cba18
Joseph <josephfabrello@gmail.com>
2022-12-30 22:28:17 +1100

    basics

---
+++ PacketSniffingAndSocketProgramming/client.py
@@ -0,0 +1,2 @@
+# Client Side for connecting to local server for chatroom
+# Clients have a unique uuid which is communicated to the server to identify themself
---
+++ PacketSniffingAndSocketProgramming/server.py
@@ -0,0 +1,31 @@
+# Server that runs a chatroom in the background
+# By Joseph Fabrello - <Insert Finish Date Here>
+# List of features:
+#   - Login and logout 
+#   - Blocking of users when failing logins
+#   - multi user
+#   - Online Prescence
+#   - When was an individual Last on
+#   - timeout (for hosts of a chatroom - admins)
+#   - broadcasting
+#   - Direct Messaging
+#   - Blocking Messages
+#   - P2p Messaging
+
+import threading
+import socket
+
+
+
+
+
+def ProcessConnection():
+
+
+def FindConnections():
+    
+
+
+
+if __name__ == "__main__":
+    FindConnections()
